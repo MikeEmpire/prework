6fd1e5f7f89034bce4880e3ddfc435c7189449c2
Mike <aolie94@gmail.com>
2017-01-07 20:09:02 -0800

    restyled html format & added semicolons to js file

--- Module-7/Broken_and_Working_Code/BrokenCode/logic.js
+++ Module-7/Broken_and_Working_Code/BrokenCode/logic.js
@@ -1,37 +1,49 @@
 $("#factButton").on("click", function() {
-	var number = Math.floor((Math.random() * booFacts.length));
-	$("#factText").text(booFacts[number])
-})
+  var number=Math.floor((Math.random() * booFacts.length));
+  $("#factText").text(booFacts[number]);
+}
 
-var booFacts = ["Boo is a pomeranian", "Boo's best friend is another pomeranian named Buddy", "Boo the Pomeranian was born on March 16", "making him a Pisces", "Boo's favourite food is grass, Boo has released two books" ]
+); 
+
+var booFacts= ["Boo is a pomeranian",
+"Boo's best friend is another pomeranian named Buddy",
+"Boo the Pomeranian was born on March 16",
+"making him a Pisces",
+"Boo's favourite food is grass, Boo has released two books"]; 
 
 // When the textPink button is pressed...
 $("#textPink").on("click", function() {
-	// Change funText to pink.
-	$("#funText").css("color", "pink")
-})
+  // Change funText to pink.
+  $("#funText").css("color", "pink");
+}
 
+); 
 // When the textOrange button is pressed...
 $("#textOrange").on("click", function() {
-	// Change funText to orange.
-	$("#funText").css("color", "orange")
-})
+  // Change funText to orange.
+  $("#funText").css("color", "orange");
+}
 
-// When the textGreen button is pressed...
+); // When the textGreen button is pressed...
 $("#textGreen").on("click", function() {
-	// Change funText to green.
-	$("#funText").css("color", "green")
-})
+  // Change funText to green.
+  $("#funText").css("color", "green");
+}
 
-
-// When the boxGrow button is clicked... 
+); // When the boxGrow button is clicked... 
 $("#boxGrow").on("click", function() {
-	// Increase the size of the box.
-	$("#box").animate({height:"+=35px", width:"+=35px"}, "fast");
-})
+  // Increase the size of the box.
+  $("#box").animate( {
+    height: "+=35px", width: "+=35px"
+  } , "fast");
+}
 
-// When the boxShrink button is clicked...
+); // When the boxShrink button is clicked...
 $("#boxShrink").on("click", function() {
-	// Decrease the size of the box.
-	$("#box").animate({height:"-=35px", width:"-=35px"}, "fast");
-})
+  // Decrease the size of the box.
+  $("#box").animate( {
+    height: "-=35px", width: "-=35px"
+  } , "fast");
+}
+
+);
