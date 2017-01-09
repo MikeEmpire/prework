acbd25446ea9477d42a0511740c476b7e688a90f
Mike <aolie94@gmail.com>
2017-01-09 01:28:30 -0800

    added functionality to button 5 and created rotate
    Initialized syntax to create functionality for button 6

--- Module-10/javascript.js
+++ Module-10/javascript.js
@@ -1,5 +1,18 @@
 // JavaScript Document
 
+function AnimateRotate(d){
+    var elem = $("#box");
+
+    $({deg: 0}).animate({deg: d}, {
+        duration: 2000,
+        step: function(now){
+            elem.css({
+                 transform: "rotate(" + now + "deg)"
+            });
+        }
+    });
+}
+
 $('#button1').on("click", function() {
 	$('#box').animate({width: 200, height: 200}, 1000);
 })
@@ -17,10 +30,12 @@ $('#button4').on("click", function() {
 	, 1000)
 })
 
-$('#button5').on("click", function() {
-	$('#box')
+ $('#button5').on("click", function() {
+    AnimateRotate(360);
 })
 
+$('#button6').on("click", function() {
+})
 
 $('#button7').on("click", function() {
 	$('#box').animate({width: 150, height: 150}, 1000).css("background-color", "orange").css("opacity", 1.0).animate({marginLeft: 0}, 1000);
