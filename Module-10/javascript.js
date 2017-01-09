6bc91011aba16632b4ab714fe3ee52b504e7a12a
Mike <aolie94@gmail.com>
2017-01-08 23:20:41 -0800

    added initial button functionality
    Also added html for curious buttons

--- Module-10/javascript.js
+++ Module-10/javascript.js
@@ -1,5 +1,27 @@
 // JavaScript Document
+
 $('#button1').on("click", function() {
 	$('#box').animate({width: 200, height: 200}, 1000);
 })
 
+$('#button2').on("click", function() {
+	$('#box').css("background-color", "blue");
+})
+
+$('#button3').on("click", function() {
+	$('#box').fadeTo("slow", 0.15);
+})
+
+$('#button4').on("click", function() {
+	$('#box')
+})
+
+/*
+$('#button7').on("click", function() {
+	$('#box').animate({width: 150, height: 150}, 1000).css("background-color", "orange").css("opacity", 1.0);
+})
+*/
+
+$('#button7').on("click", function() {
+	$('#box').removeAttr('style');
+})
