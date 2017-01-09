5dae0f07f100b11fa22b001bdf7c496ed00141a3
Mike <aolie94@gmail.com>
2017-01-09 00:40:30 -0800

    added easing.js. added custom buttons
    functionality added to button4

--- Module-10/javascript.js
+++ Module-10/javascript.js
@@ -13,15 +13,13 @@ $('#button3').on("click", function() {
 })
 
 $('#button4').on("click", function() {
-	$('#box')
+	$('#box').animate({marginLeft: 200}
+	, 1000)
 })
 
-/*
-$('#button7').on("click", function() {
-	$('#box').animate({width: 150, height: 150}, 1000).css("background-color", "orange").css("opacity", 1.0);
-})
-*/
+
 
 $('#button7').on("click", function() {
-	$('#box').removeAttr('style');
+	$('#box').animate({width: 150, height: 150}, 1000).css("background-color", "orange").css("opacity", 1.0).animate({marginLeft: 0}, 1000);
 })
+
